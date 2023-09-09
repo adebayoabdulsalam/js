d3ccdb23d4e5bbbbfeaa2d955288b0e4b44b6b79
MUHYDEEN ADEDIRAN <adediranmuhydeen@yahoo.com>
2023-09-09 19:59:09 +0100

    frist commit

---
+++ lock.js
@@ -0,0 +1,19 @@
+function jump(x,p){
+return x+p
+}
+console.log(jump(56,60))
+
+function bag(o,g){
+  return o*g
+  }
+  console.log(bag(56,60))
+
+  function boy(h,l){
+    return h-l
+    }
+    console.log(boy(100,78))
+
+    function got(s,r){
+      return s-r
+      }
+      console.log(got(56,78))
